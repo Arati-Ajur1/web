149ed514b147fd6b7e5df814057bb92e9943cdd3
Arati-Ajur1 <ajurarati008@gmail.com>
2025-11-24 17:20:10 +0530

    Update App.js

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this is new project.
+          this is Devops project.
         </p>
         <a
           className="App-link"
